e734b76dbef84af715f4dab57ae5e092ccc7d183
Bhuvnesh Kumar Sharma <80446449+BhuvneshKumarSharma@users.noreply.github.com>
2021-03-16 22:06:22 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,6 @@
+num1=int(input("enter first number\n"))
+num2=int(input("enter second number\n"))
+num3=int(input("enter third number\n"))
+print("The average of three number")
+avg=((num1)+num2+num3)/3
+print(avg)
